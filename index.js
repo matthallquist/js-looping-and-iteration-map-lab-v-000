94d5bf82832cf975c39a7617df4fec46a4c2fc16
Matthew Hallquist <matthallquist@yahoo.com>
2018-06-29 14:43:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nameToAttributes(array) {
     newArray.push(name.split(" "));
   }
   return newArray.map(function(instance) {
-    Object.assign({}, {firstName: instance[0], lastName: instance[1]});
+    console.log(Object.assign({}, {firstName: instance[0], lastName: instance[1]});)
   });
 }
