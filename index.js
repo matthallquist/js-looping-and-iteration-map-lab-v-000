28caaa175f3e94583483b14862b19729da7585fc
Matthew Hallquist <matthallquist@yahoo.com>
2018-06-29 14:22:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function lowerCaseDrivers(array) {
   return array.map(function (instance) {return instance.toLowerCase();});
 }
+
+function nameToAttributes(array) {
+  return array.map(function (instance) { return Object.assign {}, instance })
+}
